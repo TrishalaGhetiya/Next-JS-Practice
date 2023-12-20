0326b33010ea703924925d537f40975d00719736
Trishala Ghetiya <trishalaghetiya@gmail.com>
2023-12-20 18:04:44 +0530

    navigate between pages

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 const AboutUs = () => {
-    return (
-        <h1>About Page</h1>
-    )
-}
+  return (
+    <>
+      <ul>
+        <li>
+          <Link href="/about/1">Senior Developer</Link>
+        </li>
+        <li>
+          <Link href="/about/2">Backend Developer</Link>
+        </li>
+        <li>
+          <Link href="/about/3">Frontend Developer</Link>
+        </li>
+      </ul>
+    </>
+  );
+};
 
 export default AboutUs;
